1b8f564cf44df2f817fe85cf4fcc1faf7b1c0b89
CarlosM01 <carlos.maciver.reydet@gmail.com>
2024-09-16 12:26:26 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('node:http')
+const fs = require('node:fs')
+
+const { findAvailablePort } = require('./free-port.js')
+
+const desiredPort = process.env.PORT ?? 3000
+
+const processRequest = (req, res)=>{
+  console.log('Request received', req.url)
+
+  if (req.url === '/') {
+    res.statusCode = 200  //Por defecto
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('Hola Mundo')
+  } 
+  else if (req.url === '/gato'){
+    res.setHeader('Content-Type', 'image/png; charset=utf-8')
+    fs.readFile('./gato.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Serever Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  }
+}
+
+
+const server = http.createServer(processRequest)
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, ()=>{
+    console.log(`Server listening on port http://localhost:${port}`)
+  })
+})
